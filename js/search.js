e856c91628935299e98598a7e2aa8e16074aff62
Ashish Pant <ashish@arpant.com>
2017-05-06 08:25:45 -0400

    Layout changes and added new javascript

--- js/search.js
+++ js/search.js
@@ -47,6 +47,7 @@ class Search {
         var PhaseMatchArray         = '';
         var LengthMatchArray        = '';
         var DataMatchArray          = '';
+        var JacketArray             = '';
 
         if(platformType==0){
             var checked                 = 'checked';
@@ -110,11 +111,21 @@ class Search {
             });
             form_param = form_param+('<div class="form-group"><label for="phase_mat">Phase Matching  <small>*</small></label><select name="phase_mat" class="form-control" id="phase_mat">' + PhaseMatchArray + '</select></div>');
 
+
+            $.each(response["jacketList"], function(ind, jacketList) {
+                //console.log(connectorList);
+                JacketArray = JacketArray+"<option value='"+(jacketList.id)+"'>"+(jacketList.jacket_type)+"</option>";
+            });
+            form_param = form_param+('<div class="form-group"><label for="jacket_type">Jacket <small>*</small></label><select name="jacket_type" class="form-control" id="jacket_type">' + JacketArray + '</select></div>');
+
             $.each(response["testData"], function(ind, testData) {
                 //console.log(connectorList);
                 DataMatchArray = DataMatchArray+"<option value='"+(testData.id)+"'>"+(testData.test_data_title)+"</option>";
             });
             form_param = form_param+('<div class="form-group"><label for="test_data_val">Test Data  <small>*</small></label><select name="test_data_val" class="form-control" id="test_data_val">' + DataMatchArray + '</select></div>');
+
+
+
             form_param = form_param+('<div class="form-group"><input type="button" onclick="SearchAPI.FormGenerator();" name="submit_frm" id="submit_frm" value="Submit" class="btn btn-success" />&nbsp;<input type="submit" name="res" id="res" value="Clear" class="btn btn-default" /></div></form>')
 
             $("#search_result").html(form_param);
@@ -134,7 +145,8 @@ class Search {
         var cableCheck              = '';
         var lengthCheck             = '';
         var phaseCheck              = '';
-        var testCheck              = '';
+        var testCheck               = '';
+        var jacketCheck             = '';
 
         var connectorsArray1        = '';
         var connectorsArray2        = '';
@@ -142,6 +154,7 @@ class Search {
         var PhaseMatchArray         = '';
         var LengthMatchArray        = '';
         var DataMatchArray          = '';
+        var JacketArray             = '';
 
         /**
          * data {string}; contains all the elements from the form.
@@ -174,6 +187,7 @@ class Search {
         var phase_mat           = obj.phase_mat;
         var lengthList          = obj.lengthList;
         var test_data_val       = obj.test_data_val;
+        var jacket_type        = obj.jacket_type;
 
         if(diameter_cable==0){
             var checked                 = 'checked';
@@ -190,6 +204,168 @@ class Search {
         }
 
 
+        /**
+         * this ajax call will fetch all the details of the elements posted from the form
+         */
+
+        $.ajax({
+            type          : "POST",
+            url           : url+'loadDescription.php',
+            contentType   : "application/x-www-form-urlencoded;",
+            dataType: "json",
+            async: false,
+            data          : { "diameter_cable":diameter_cable,
+                                "cable_name":cable_name,
+                                'connector_1':connector_1,
+                                'connector_2':connector_2,
+                                'diameter_cable':diameter_cable,
+                                'length_dia':length_dia,
+                                'phase_mat':phase_mat,
+                                'lengthList':lengthList,
+                                'jacket_type':jacket_type,
+                                'test_data_val':test_data_val}
+        }).done(function(response){
+                //console.log(response);
+            var technical_spec      = '';
+            var PlatForm            = '';
+            var Connector1          = '';
+            var Connector2          = '';
+            var CableSegB           = '';
+            var CableSegC           = '';
+            var Conn1SegB           = '';
+            var Conn2SegB           = '';
+            var JacketSegA          = '';
+            var LengthABet          = '';
+            var LengthSecC          = '';
+            var PhaseSegA           = '';
+            var TestSegA            = '';
+            var DescSegA1           = '';
+            var DescSegB1           = '';
+            var DescFreq1           = '';
+
+            var DescSegA2           = '';
+            var DescSegB2           = '';
+            var DescFreq2           = '';
+
+            var CableDescA          = '';
+            var CableDescB          = '';
+            var CableDescC          = '';
+
+            var LengthDescB         = '';
+            var LengthDescA         = '';
+
+            var DescRotation        = '';
+            var JacketDescA         = '';
+            var TestDescA           = '';
+            var PhaseDescA          = '';
+
+            var ConnDescB1Commas    = '';
+
+            var PriceCap            = '';
+
+            switch(diameter_cable){
+                case '0':
+                    PlatForm = '086';
+                    break;
+                case '1':
+                    PlatForm = '047';
+                    break;
+                case '2':
+                    PlatForm = '141';
+                    break;
+            }
+
+
+            //
+            technical_spec = ('<div class="page-header"><h4>TECHNICAL SPECS</h4></div>');
+
+            $.each(response["connector_1"], function(ind, connectorDet1) {
+                Connector1  = connectorDet1['pn_seg_a'];
+                Conn1SegB   = connectorDet1['pn_seg_b'];
+                DescSegA1   = connectorDet1['desc_seg_a'];
+                DescSegB1   = connectorDet1['desc_seg_b'];
+                DescFreq1   = connectorDet1['frequency_range'];
+            });
+            $.each(response["connector_2"], function(ind, connectorDet2) {
+                Connector2  = connectorDet2['pn_seg_a'];
+                Conn2SegB   = connectorDet2['pn_seg_b'];
+                DescSegA2   = connectorDet2['desc_seg_a'];
+                DescSegB2   = connectorDet2['desc_seg_b'];
+                DescFreq2   = connectorDet2['frequency_range'];
+            });
+            $.each(response["cableList"], function(ind, CableDet) {
+                CableSegB   = CableDet['pn_seg_b'];
+                CableSegC   = CableDet['pn_seg_c'];
+                CableDescA  = CableDet['desc_seg_a'];
+                CableDescB  = CableDet['desc_seg_b'];
+                CableDescC  = CableDet['desc_seg_c'];
+            });
+            $.each(response["jacketList"], function(ind, JacketDet) {
+                JacketSegA   = JacketDet['pn_seg_a'];
+                JacketDescA   = JacketDet['desc_seg_a'];
+            });
+
+            $.each(response["lengthList"], function(ind, LengthDet) {
+                LengthABet   = LengthDet['pn_seg_a'];
+                LengthSecC   = LengthDet['pn_seg_c'];
+                LengthDescB  = LengthDet['desc_seg_b'];
+            });
+
+            $.each(response["phaseMatchList"], function(ind, PhaseMatchDet) {
+                PhaseSegA   = PhaseMatchDet['pn_seg_a'];
+                PhaseDescA  = PhaseMatchDet['desc_seg_a'];
+            });
+
+            $.each(response["testData"], function(ind, TestDataDet) {
+                TestSegA   = TestDataDet['pn_seg_a'];
+                TestDescA   = TestDataDet['desc_seg_a'];
+            });
+
+            $.each(response["discountList"], function(ind, DiscountDet) {
+                PriceCap = PriceCap+', '+DiscountDet['range_discount']+' $'+DiscountDet['discount_percentage'];
+            });
+
+            var PartNumber = '';
+
+            var Description = '';
+
+            if (DescFreq1 > DescFreq2){
+                ConnDescB1Commas = DescSegB2;
+            } else {
+                ConnDescB1Commas = DescSegB1;
+            }
+
+
+
+            PartNumber = Connector1+Connector2+PlatForm+Conn1SegB+Conn2SegB+CableSegB+JacketSegA+LengthABet+length_dia+PhaseSegA+CableSegC+TestSegA;
+
+
+            Description = 'RF Cable Assembly, '+DescSegA1+' to '+DescSegA2+DescRotation+' ,'
+                + ' '+CableDescA+' '+CableDescB+' ,'+ConnDescB1Commas+' ,' +
+                ''+JacketDescA+', '+PhaseDescA+', '+length_dia+' '+LengthDescB+' , '+CableDescC+' '+TestDescA;
+
+
+
+            //$description_generator = "RF Cable Assembly, " .
+            // $connector_des_a_1 . " to " . $connector_des_a_2 .
+            // $description_rotation . $cabletype_des_a . " " .
+            // $cabletype_des_b . $connector_des_b_1_commas .
+            // $jacket_des_a . $phase_mat_des_a . " " .
+            // $_SESSION['config']['length_dia'] .
+            // $des_length . " " . $length_des_b .
+            // $cabletype_des_c . $test_des_a;
+
+
+
+
+            $('.nav a[href="#search_result_array"]').tab('show');
+            technical_spec = technical_spec+'<div><strong>Part Number:</strong> '+PartNumber+'</div>';
+            technical_spec = technical_spec+'<div><strong>Description:</strong> '+Description+'</div>';
+            technical_spec = technical_spec+'<div><strong>Price:</strong> '+Description+'</div>';
+
+            $("#search_result_array").html(technical_spec);
+
+        });
 
         $.ajax({
             type          : "POST",
@@ -274,6 +450,19 @@ class Search {
             });
             form_param = form_param+('<div class="form-group"><label for="phase_mat">Phase Matching  <small>*</small></label><select name="phase_mat" class="form-control" id="phase_mat">' + PhaseMatchArray + '</select></div>');
 
+            $.each(response["jacketList"], function(ind, jacketList) {
+
+                if(jacketList.id==jacket_type){
+                    jacketCheck = "selected";
+                }else{
+                    jacketCheck = "";
+                }
+                //console.log(connectorList);
+                JacketArray = JacketArray+"<option value='"+(jacketList.id)+"' "+jacketCheck+">"+(jacketList.jacket_type)+"</option>";
+            });
+            form_param = form_param+('<div class="form-group"><label for="jacket_type">Jacket <small>*</small></label><select name="jacket_type" class="form-control" id="jacket_type">' + JacketArray + '</select></div>');
+
+
             $.each(response["testData"], function(ind, testData) {
                 //console.log(connectorList);
                 if(testData.id==test_data_val){
@@ -286,6 +475,7 @@ class Search {
             });
             form_param = form_param+('<div class="form-group"><label for="test_data_val">Test Data  <small>*</small></label><select name="test_data_val" class="form-control" id="test_data_val">' + DataMatchArray + '</select></div>');
             form_param = form_param+('<div class="form-group"><input type="button" onclick="SearchAPI.FormGenerator();" name="submit_frm" id="submit_frm" value="Submit" class="btn btn-success" />&nbsp;<input type="submit" name="res" id="res" value="Clear" class="btn btn-default" /></div></form>')
+
 
             $("#search_result").html(form_param);
 
